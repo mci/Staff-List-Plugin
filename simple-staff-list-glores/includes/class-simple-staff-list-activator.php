fe286de9ccf34853cee2090151a956afc4ec90e2
Russell Moffitt <russmo@coralhead.com>
2018-01-23 14:58:28 -0800

    GLORES version 2.0.2 release final updating default templates

--- simple-staff-list-glores/includes/class-simple-staff-list-activator.php
+++ simple-staff-list-glores/includes/class-simple-staff-list-activator.php
@@ -33,73 +33,94 @@ class Simple_Staff_List_Activator {
 	public static function activate( $is_forced = false ) {
 		$default_template = '
 		[staff_loop]
-			<img class="staff-member-photo" src="[staff-photo-url]" alt="[staff-name] : [staff-position]">
-			<div class="staff-member-info-wrap">
-				[staff-name-formatted]
-				[staff-position-formatted]
-				[staff-bio-formatted]
-				[staff-email-link]
-			</div>
+		    <li class="staff-bio person">
+		        <div class="image-wrapper" style="background-image: url([staff-photo-url])" title="[staff-name]">
+		            <i></i>
+		        </div>
+		        <div class="staff-name-title">
+		            <h4>[staff-name]</h4>
+		            <p class="job-title">[staff-position]</p>
+		        </div>
+		        <div class="staff-info modal modal-valign fade in" style="display:none;" tabindex="-1" role="dialog" aria-labelledby="staffPopupLabel" aria-hidden="false">
+		            <div class="modal-dialog">
+		                <div class="modal-content">
+		                    <div class="modal-header">
+		                        <button type="button" class="btn-md close modal-close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+		                        <div class="modal-image-wrapper" style="background-image: url([staff-photo-url])"></div>
+		                        <div class="modal-title">
+		                            <h4>[staff-name]</h4>
+		                            <p class="job-title">[staff-position]</p>
+		                        </div>
+		                    </div>
+		                    <div class="modal-body">
+		                        <div class="modal-blurb bio-blurb">
+		                            [staff-bio]
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </li>
 		[/staff_loop]';
 
 		$default_css = '
-			/*  div wrapped around entire staff list  */
-			div.staff-member-listing {
-			}
-			/*  div wrapped around each staff member  */
-			div.staff-member {
-				padding-bottom: 2em;
-				border-bottom: thin dotted #aaa;
-			}
-			/*  "Even" staff member  */
-			div.staff-member.even {
-			}
-			/*  "Odd" staff member  */
-			div.staff-member.odd {
-				margin-top: 2em;
-			}
-			/*  Last staff member  */
-			div.staff-member.last {
-				padding-bottom: 0;
-				border: none;
-			}
-			/*  Wrap around staff info  */
-			.staff-member-info-wrap {
-				float: left;
-				width: 70%;
-				margin-left: 3%;
-			}
-			/*  [staff-bio-formatted]  */
-			div.staff-member-bio {
-			}
-			/*  p tags within [staff-bio-formatted]  */
-			div.staff-member-bio p {
-			}
-			/*  [staff-photo]  */
-			img.staff-member-photo {
-				float: left;
-			}
-			/*  [staff-email-link]  */
-			.staff-member-email {
-			}
-			/*  [staff-name-formatted]  */
-			div.staff-member-listing h3.staff-member-name {
-				margin: 0;
-			}
-			/*  [staff-position-formatted]  */
-			div.staff-member-listing h4.staff-member-position {
-				margin: 0;
-				font-style: italic;
-			}
-			/* Clearfix for div.staff-member */
-			div.staff-member:after {
-				content: "";
-				display: block;
-				clear: both;
-			}
-			/* Clearfix for <= IE7 */
-			* html div.staff-member { height: 1%; }
-			div.staff-member { display: block; }
+			// /*  div wrapped around entire staff list  */
+			// div.staff-member-listing {
+			// }
+			// /*  div wrapped around each staff member  */
+			// div.staff-member {
+			// 	padding-bottom: 2em;
+			// 	border-bottom: thin dotted #aaa;
+			// }
+			// /*  "Even" staff member  */
+			// div.staff-member.even {
+			// }
+			// /*  "Odd" staff member  */
+			// div.staff-member.odd {
+			// 	margin-top: 2em;
+			// }
+			// /*  Last staff member  */
+			// div.staff-member.last {
+			// 	padding-bottom: 0;
+			// 	border: none;
+			// }
+			// /*  Wrap around staff info  */
+			// .staff-member-info-wrap {
+			// 	float: left;
+			// 	width: 70%;
+			// 	margin-left: 3%;
+			// }
+			// /*  [staff-bio-formatted]  */
+			// div.staff-member-bio {
+			// }
+			// /*  p tags within [staff-bio-formatted]  */
+			// div.staff-member-bio p {
+			// }
+			// /*  [staff-photo]  */
+			// img.staff-member-photo {
+			// 	float: left;
+			// }
+			// /*  [staff-email-link]  */
+			// .staff-member-email {
+			// }
+			// /*  [staff-name-formatted]  */
+			// div.staff-member-listing h3.staff-member-name {
+			// 	margin: 0;
+			// }
+			// /*  [staff-position-formatted]  */
+			// div.staff-member-listing h4.staff-member-position {
+			// 	margin: 0;
+			// 	font-style: italic;
+			// }
+			// /* Clearfix for div.staff-member */
+			// div.staff-member:after {
+			// 	content: "";
+			// 	display: block;
+			// 	clear: both;
+			// }
+			// /* Clearfix for <= IE7 */
+			// * html div.staff-member { height: 1%; }
+			// div.staff-member { display: block; }
 		';
 
 		$default_tags       = array(
